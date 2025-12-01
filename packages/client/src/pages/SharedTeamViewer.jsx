1a033a5ff78dc431085ae5c2e9cad2210cbf3b99
Castilho <castilho@castleventures.ca>
2025-12-01 07:00:13 -0500

    fix: add missing useMemo import in SharedTeamViewer

--- packages/client/src/pages/SharedTeamViewer.jsx
+++ packages/client/src/pages/SharedTeamViewer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, useMemo } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import api from '../api/axios';
